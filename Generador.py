aec5f803dfbbc4795250b700e5240e489c9182a2
Francisco97M <francisco.morales@enroutesystems.com>
2021-11-03 12:05:46 -0600

    Ejercicios

---
+++ Generador.py
@@ -0,0 +1,3 @@
+gen = (i for i in range(100) if i%2!=0)
+for x in gen:
+    print(x)
--- Prueba.py
+++ Prueba.py
@@ -37,3 +37,71 @@ while bol != bol2:
     print(var_rapida)
     var_rapida += 1
 
+gen = (i for i in range(10) if i%2!=0)
+for x in gen:
+    print(x)
+
+arreglo_num = [0, 1, 2, 3]
+arreglo_num.append(4)
+arreglo_num.extend([5, 6])
+print(arreglo_num)
+
+tupla = (1, 2, 3)
+print(tupla)
+
+comp = [i+1 for i in range(100) if i%2!=0]
+comp.append(101)
+print(comp)
+
+dic = {'color': 'red', 'size': 123, 'width': 405.60}
+for key in dic:
+    print(key)
+    print(dic[key])
+
+if 'color' in dic:
+    print(dic['color'])
+
+def multiplicacion(a1,a2):
+    return a1 * a2
+
+def mult(a1,a2, i = 0, acu=0):
+    if i < a2:
+        return mult(a1,a2,i+1,a1+acu)
+    else:
+        return acu
+print(multiplicacion(2,6))
+
+print(mult(2,6))
+
+def mult2(*args):
+    res = 1
+    for i in args:
+        res *= i
+
+    return res
+print(mult2(2,6))
+print(mult2(2,6,4,3))
+
+def mult3(**kwargs):
+    res = 1
+    for key in kwargs:
+        print(key)
+        res *= kwargs[key]
+
+    return res    
+
+print(mult3(num1 = 2, num2 = 6))
+print(mult3(num1 = 2, num2 = 6, num3 = 4, num4 = 3))
+
+def mult4(*args, **kwargs):
+    res = 1
+    for key in kwargs:
+        print(key)
+        res *= kwargs[key]
+    
+    for i in args:
+        res *= i
+
+    return
+
+print(mult4(4, 3, num1 = 2, num2 = 6))
